661f4f3dcd0d7e8d95a841698655824adace9ac2
Alex Belisle Turcot <alex.belisleturcot@gmail.com>
2016-07-11 09:12:12 -0400

    add config-server

--- src/main/java/com/alexbt/mongodb/Launcher.java
+++ src/main/java/com/alexbt/mongodb/Launcher.java
@@ -2,8 +2,10 @@ package com.alexbt.mongodb;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Launcher {
 	
 	public static void main(String[] args){
