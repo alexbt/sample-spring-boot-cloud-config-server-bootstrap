ec3dde1b2866f5c384d5b30b57f1d7a5db65a94b
Alex Belisle Turcot <alex.belisleturcot@gmail.com>
2016-07-11 22:41:47 -0400

    initial

--- src/main/java/com/alexbt/mongodb/CloudController.java
+++ src/main/java/com/alexbt/mongodb/CloudController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cloud")
 public class CloudController {
 
-	@Value("${env}")
+	@Value("${testproperty}")
 	private String env;
 	
 	@RequestMapping(method = RequestMethod.GET)
